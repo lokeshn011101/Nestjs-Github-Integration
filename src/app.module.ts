5a62ccd969a46234fccd3b04e7bbf95a9b91fa9d
lokeshn011101 <lokesh19055@cse.ssn.edu.in>
2022-09-10 19:43:20 +0530

    Add routes for Github integration

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'userDB',
+      entities: [User],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
